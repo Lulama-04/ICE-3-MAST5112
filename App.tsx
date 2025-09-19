58f5044c1cfe1d1b8671b61d25565bada2e6041c
Lulama <lulamasangweni047@gmail.com>
2025-09-19 22:40:39 +0300

    Changed the results text color and set dog as a default value

--- App.tsx
+++ App.tsx
@@ -3,25 +3,25 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
  
 export default function App() {
-  const [color, setColor] = useState('red'); // default value
+  const [animal, setColor] = useState('dog'); // default value
  
   return (
 <View style={styles.container}>
-<Text style={styles.title}>Select Your Favorite Color:</Text>
+<Text style={styles.title}>Select Your Favorite Animal:</Text>
  
       <Picker              //use this to make a drop box 
-        selectedValue={color}
+        selectedValue={animal}
         onValueChange={(itemValue) => setColor(itemValue)}
         style={styles.picker}
         >
-          <Picker.Item label="Lion" value="red" />
-          <Picker.Item label="Dolphin" value="blue" />
-          <Picker.Item label="Cheetah" value="Orange" />
-          <Picker.Item label="Eagle" value="yellow" />
-          <Picker.Item label="Elephant" value="Black" />
+          <Picker.Item label="dog" value="dog" />
+          <Picker.Item label="Dolphin" value="Dolphin" />
+          <Picker.Item label="Cheetah" value="Cheetah" />
+          <Picker.Item label="Eagle" value="Eagle" />
+          <Picker.Item label="Elephant" value="Elephant" />
           </Picker>
           
-          <Text style={styles.result}>Your Favourite animal is: {color}</Text>
+          <Text style={styles.result}>Your Favourite animal is a: {animal}</Text>
         </View>
   );
 }
@@ -30,6 +30,6 @@ const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 },
   title: { fontSize: 20, marginBottom: 20 },
   picker: { height: 50, width: 200 },
-  result: { marginTop: 20, fontSize: 18, color: 'Green' },
+  result: { marginTop: 20, fontSize: 18, color: 'green' },
 });
 
